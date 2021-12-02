d00b442e6d5e5354299af90538128eda9f27b8e0
Yoelptr <yoelenggong10@gmail.com>
2021-12-02 08:09:35 +0700

    Add project files.

---
+++ RegisterPages/Model/E=RegisterUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegisterPages.Model
+{
+    class E_RegisterUser
+    {
+        public event EventHandler registerUserEvent;
+        public void RegisterAUser()
+        {
+            Console.WriteLine("User registered");
+            if (registerUserEvent != null)
+            {
+                registerUserEvent(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ RegisterPages/Model/WhatsappVerification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegisterPages.Model
+{
+    class WhatsappVerification
+    {
+        public event EventHandler waVerificationEvent;
+        public void OnUserRegistered(object source, EventArgs e)
+        {
+            //any process and logic here...
+            waVerificationEvent(this, EventArgs.Empty);
+        }
+    }
+}
